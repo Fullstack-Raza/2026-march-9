2e9904d1103288c433dcd2c732af9dbd4e76dcb4
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-03-11 17:58:45 +0500

    game v1 js

--- js.js
+++ js.js
@@ -8,13 +8,46 @@
 یوزر ایک نمبر دے گا 
 پھر ایک رزلٹ شو ہوگا کہ اپکا نمبر بڑا ہے یا چھوٹا ہے کومپیوٹر کے نمبر 
 تا کے تم اس نمبر پہنچ سکو
-
-
-
-
-
-
 */
+
+const start = document.querySelector(".start");
+const gameDis = document.querySelector(".game-dis")
+const game = document.querySelector(".game")
+const gameResul = document.querySelector(".game-result")
+const inp = document.querySelector(".game input")
+const btn = document.querySelector(".game button")
+const attmpt = document.querySelector("#attmpt")
+const win = document.querySelector("#win")
+const lose = document.querySelector("#lose")
+const totale = document.querySelector("#totale")
+let Random = Math.floor(Math.random()*101);
+localStorage.setItem("win",win.textContent);
+
+
+start.addEventListener("click",function(){
+    gameDis.style.display="inherit";
+    game.style.display="inherit"
+    start.style.display="none"  
+    attmpt.textContent = 10;
+})
+
+btn.addEventListener("click",function(){
+    gameResul.style.display="inherit"
+    if(!inp.value){
+        gameResul.innerHTML=`<p>Give Number Please</p>`
+    }else if(inp.value > Random){
+       gameResul.innerHTML=`<p>Try Smaller Number</p>`
+   }else if (inp.value < Random){
+      gameResul.innerHTML=`<p>Try Bigger Number</p>`
+   }else {
+     gameResul.innerHTML=`<p>Wah Wah</p>`;
+   }
+})
+
+
+
+
+
 
 
 
